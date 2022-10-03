2e3c4f242a9878013126b8752e702ab862309ebb
Naru-hub <1208ny0324@gmail.com>
2022-10-03 20:30:34 +0900

    react_basic 後半

--- 04_react_basic/src/120_props_rules/start/components/Hello.js
+++ 04_react_basic/src/120_props_rules/start/components/Hello.js
@@ -1,4 +1,11 @@
 const Hello = (props) => {
+  // POINT propsは読み取り専用
+  // props.name = 'Bob';
+
+  const desc = Reflect.getOwnPropertyDescriptor(props, 'name');
+  console.log(desc);
+  
+  // console.log(props.name);
   return (
     <div>
       <h3>Hello {props.name}</h3>
