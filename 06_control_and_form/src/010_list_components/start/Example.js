7120b3496be5f9ee7d83350b962830bcb2022282
Naru-hub <1208ny0324@gmail.com>
2022-10-26 07:04:48 +0900

    list_components

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -1,13 +1,22 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const animalList = [];
+  for (const animal of animals) {
+    animalList.push(<li>{animal}</li>);
+  }
+
+  const helloAnimals = animals.map((animal) => <li>Hello, {animal}</li>);
 
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* {animalList}
+        {helloAnimals} */}
+        {animals.map((animal) => (
+          <li>Hello, {animal}</li>
+        ))}
       </ul>
     </>
   );
