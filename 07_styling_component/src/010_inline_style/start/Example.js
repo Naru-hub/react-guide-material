3a1923c1bf89a333fd3a8b743f2e648a8e5a0f1a
Naru-hub <1208ny0324@gmail.com>
2022-11-02 20:43:11 +0900

    inline_style

--- 07_styling_component/src/010_inline_style/start/Example.js
+++ 07_styling_component/src/010_inline_style/start/Example.js
@@ -1,16 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Example = () => {
-    const [isSelected, setIsSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
-    const clickHandler = () => setIsSelected(prev => !prev);
+  const clickHandler = () => setIsSelected((prev) => !prev);
 
-    return (
-        <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
-        </>
-    )
+  const style = {
+    width: 120,
+    height: 60,
+    display: "block",
+    fontWeight: "bold",
+    "border-radius": 9999,
+    cursor: "pointer",
+    border: "none",
+    margin: "auto",
+    background: isSelected ? "pink" : "",
+    // ::before, ::after, :hover, :activeなど擬似要素や擬似セレクタはインラインスタイル記法では使えない
+    // @media (min-width: 600px) {} などのメディアクエリも使えない
+  };
+
+  return (
+    <>
+      <button onClick={clickHandler} style={style}>
+        ボタン
+      </button>
+      <button style={style}>ボタン2</button>
+      <div style={{textAlign: "center"}}>{isSelected && "クリックされました。"}</div>
+    </>
+  );
 };
 
 export default Example;
