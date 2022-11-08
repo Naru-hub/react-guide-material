e42e8916a932525cd120198b86e217c1f7be5396
Naru-hub <1208ny0324@gmail.com>
2022-11-08 16:54:52 +0900

    09_debugging/src/020_debugging and 10_functional_programming/src/010_data_procedure

--- 10_functional_programming/src/010_data_procedure/start/Example.js
+++ 10_functional_programming/src/010_data_procedure/start/Example.js
@@ -1,6 +1,25 @@
 const Example = () => {
+  // 関数型
+  const nums = [1, 2, 3];
+  const sum = (arry) => arry.reduce((accu, curr) => accu + curr);
+
+  // オブジェクト指向型
+  const numObj = {
+    nums: [1, 2, 3],
+    sum() {
+      const nums = this.nums;
+      let sumValue = 0;
+      for (let i = 0; i < nums.length; i++) {
+        sumValue += nums[i];
+      }
+      return sumValue;
+    },
+  };
   return (
     <>
+      <div>オブジェクト指向型: {numObj.sum()}</div>
+      <div>関数型: {sum(nums)}</div>
+
       <h3>状態管理と処理を分離</h3>
       <p>状態（データ）と処理（やりたいこと）は切り離す</p>
     </>
