5858aa4d1be605997821b80c71a57c821e2dee30
Naru-hub <1208ny0324@gmail.com>
2022-12-16 21:04:54 +0900

    useContextのリファクタリング

--- 11_hooks_p1/src/050_context_file/start/Example.js
+++ 11_hooks_p1/src/050_context_file/start/Example.js
@@ -1,21 +1,17 @@
-import { createContext, useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import "./Example.css";
-
-export const ThemeContext = createContext();
+import { ThemeProvider } from "./context/ThemeContext";
 
 const Example = () => {
-  const [theme, setTheme] = useState("light");
-
-  
-
-  return <>
-  <ThemeContext.Provider value={[theme, setTheme]}>
-    <Header />
-    <Main />
-  </ThemeContext.Provider>
-  </>;
+  return (
+    <>
+      <ThemeProvider>
+        <Header />
+        <Main />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default Example;
